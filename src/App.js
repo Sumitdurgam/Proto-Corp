84b82e9f0af8179ee1cc1782aa78590e39e0294f
Sumitdurgam <sumitdurgam12@gmail.com>
2025-11-24 14:54:09 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { useRef } from "react";
-import VideoPlayer from "./Components/VideoPlayer"
+import VideoPlayer from "./Components/VideoPlayer";
 import "./App.css";
 
 export default function App() {
   const videoRefs = useRef([]);
 
   const streams = [
-    "http://localhost/hls/stream1.m3u8",
-    "http://localhost/hls/stream2.m3u8",
-    "http://localhost/hls/stream3.m3u8",
-    "http://localhost/hls/stream4.m3u8",
-    "http://localhost/hls/stream5.m3u8",
-    "http://localhost/hls/stream6.m3u8"
+    "http://localhost/hls/live1.m3u8",
+    "http://localhost/hls/live2.m3u8",
+    "http://localhost/hls/live3.m3u8",
+    "http://localhost/hls/live4.m3u8",
+    "http://localhost/hls/live5.m3u8",
+    "http://localhost/hls/live6.m3u8",
   ];
 
   const handleSync = () => {
     const masterTime = videoRefs.current[0]?.currentTime;
 
-    videoRefs.current.forEach(video => {
+    videoRefs.current.forEach((video) => {
       if (video && Math.abs(video.currentTime - masterTime) > 0.3) {
-        video.currentTime = masterTime; // Adjust playback positions
+        video.currentTime = masterTime;
+        // Adjust playback positions
       }
     });
   };
